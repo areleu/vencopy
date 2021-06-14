1379126469eec6ae53afde4af82457c030e3260d
Miorelli <Fabia.Miorelli@dlr.de>
2021-06-14 17:50:09 +0200

    doc template theme change

--- source/conf.py
+++ source/conf.py
@@ -12,6 +12,7 @@
 #
 import os
 import sys
+import sphinx_rtd_theme
 sys.path.insert(0, os.path.abspath('.'))
 sys.path.append(os.path.abspath('..'))
 sys.path.append(os.path.abspath('../scripts'))
@@ -20,7 +21,7 @@ sys.path.append(os.path.abspath('../scripts'))
 # -- Project information -----------------------------------------------------
 
 project = 'VencoPy'
-copyright = '2020, Niklas Wulff'
+copyright = '2020-2021, Niklas Wulff'
 author = 'Niklas Wulff'
 
 # The full version, including alpha/beta/rc tags
@@ -32,8 +33,7 @@ release = 'March 2020'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc'
-]
+extensions = ['sphinx.ext.autodoc', 'myst_parser', 'sphinx_rtd_theme']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -49,7 +49,8 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
+html_theme_path = ['_themes', ]
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
