63c3f226cb2b0a9218cdb2b9ef5f2f3a0b77f81d
Miorelli <Fabia.Miorelli@dlr.de>
2021-06-17 13:30:03 +0200

    updated readme and docu template

--- source/conf.py
+++ source/conf.py
@@ -61,8 +61,9 @@ html_theme_options = {
     'includehidden': True,
     'titles_only': False
 }
+
 # html_css_files = ['style.css']
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-# html_static_path = ['_static']
+html_static_path = ['_static']
