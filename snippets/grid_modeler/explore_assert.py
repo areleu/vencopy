070e8c834692d8f90b377399c88b8a22cbe451ce
wulf_ni <niklas.wulff@dlr.de>
2022-10-21 14:58:11 +0200

    Adding first assert test for homeProbabilityDistribution

---
+++ snippets/grid_modeler/explore_assert.py
@@ -0,0 +1,6 @@
+a = 1
+
+while a > 0:
+	assert isinstance(a, int), 'FAIL'
+
+# Assertion cancels if the condition fails
--- snippets/grid_modeler/explore_gridModeler.py
+++ snippets/grid_modeler/explore_gridModeler.py
@@ -6,11 +6,12 @@ from pathlib import Path
 activities = pd.DataFrame()
 purposes = ['DRIVING', 'HOME', 'WORK', 'SCHOOL', 'SHOPPING', 'LEISURE', 'OTHER']
 activities['purposeStr'] = purposes
+activities['hhID'] = range(len(purposes))
 
 basePath = Path(__file__).parent.parent.parent/'vencopy'
 datasetID = "MiD17"
 configNames = ("globalConfig", "localPathConfig", "parseConfig", "diaryConfig",
-                "gridConfig", "flexConfig", "aggregatorConfig", "evaluatorConfig")
+               "gridConfig", "flexConfig", "aggregatorConfig", "evaluatorConfig")
 configDict = loadConfigDict(configNames, basePath=basePath)
 vpGrid = GridModeler(configDict=configDict, datasetID=datasetID, activities=activities, gridModel='simple')
 vpGrid.assignGrid()
@@ -38,7 +39,11 @@ print(type(power_values_df))
 print(type(simple_power_values_config))
 
 if power_values_df.difference(simple_power_values_config):
-   print('Not valid values')
+    print('Not valid values')
 else:
     print('Valid values')
+
+# _homeProbabilityDistribution
+print('hhID' in vpGrid.activities.columns)
+assert 'hhID' in vpGrid.activities.columns, 'hhID is not in the columns of the class'
 
