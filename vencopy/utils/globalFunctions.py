eec4857e6aac52fe6d26e247e8bd8d282ca98e12
Fabia Miorelli <fabia.miorelli@dlr.de>
2022-12-01 11:28:34 +0100

    renames paramenters in writeOut and createFileName

--- vencopy/utils/globalFunctions.py
+++ vencopy/utils/globalFunctions.py
@@ -96,8 +96,8 @@ def createOutputFolders(configDict: dict):
             os.mkdir(Path(root / mainDir / subDir))
 
 
-def createFileString(globalConfig: dict, fileKey: str, datasetID: str, manualLabel: str = '',
-                     filetypeStr: str = 'csv'):
+def createFileName(globalConfig: dict, fileNameID: str, datasetID: str, manualLabel: str = '',
+                     suffix: str = 'csv'):
     """
     Generic method used for fileString compilation throughout the VencoPy framework. This method does not write any
     files but just creates the file name including the filetype suffix.
@@ -110,9 +110,9 @@ def createFileString(globalConfig: dict, fileKey: str, datasetID: str, manualLab
     :return: Full name of file to be written.
     """
     if datasetID is None:
-        return f"{globalConfig['files'][fileKey]}_{globalConfig['labels']['runLabel']}_{manualLabel}.{filetypeStr}"
-    return f"{globalConfig['files'][datasetID][fileKey]}_{globalConfig['labels']['runLabel']}_{manualLabel}" \
-           f"{datasetID}.{filetypeStr}"
+        return f"{globalConfig['files'][fileNameID]}_{globalConfig['labels']['runLabel']}_{manualLabel}.{suffix}"
+    return f"{globalConfig['files'][datasetID][fileNameID]}_{globalConfig['labels']['runLabel']}_{manualLabel}" \
+           f"{datasetID}.{suffix}"
 
 
 def mergeVariables(data, variableData, variables):
@@ -160,7 +160,7 @@ def writeProfilesToCSV(profileDictOut, globalConfig: dict, localPathConfig: dict
         dataOut = pd.DataFrame(profileDictOut)
         dataOut.to_csv(Path(
             localPathConfig['pathAbsolute']['vencoPyRoot']) / globalConfig['pathRelative']['flexOutput'] /
-            createFileString(globalConfig=globalConfig,
+            createFileName(globalConfig=globalConfig,
                              fileKey='output',
                              manualLabel=globalConfig['labels']['technologyLabel'],
                              datasetID=datasetID),
@@ -175,7 +175,7 @@ def writeProfilesToCSV(profileDictOut, globalConfig: dict, localPathConfig: dict
 
 def writeOut(data: pd.DataFrame, path: Path):
     data.to_csv(path)
-    print(f'Dataset written to {path}')
+    print(f'Dataset written to {path}.')
 
 
 def dumpReferenceData(data: pd.DataFrame, tag: str, path: Path):
