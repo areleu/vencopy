d78267d0f3817fffc1bfa0d139f96a39f8743f33
Fabia Miorelli <fabia.miorelli@dlr.de>
2023-03-23 11:11:59 +0100

    implements dataParse function to delegate data paraing

--- vencopy/run.py
+++ vencopy/run.py
@@ -15,37 +15,51 @@ if __package__ is None or __package__ == "":
     sys.path.append(path.dirname(path.dirname(__file__)))
 
 from pathlib import Path
-from vencopy.core.dataParsers import ParseMiD, ParseKiD, ParseVF
+from vencopy.core.dataParsers import parseData
 from vencopy.core.diaryBuilders import WeekDiaryBuilder, DiaryBuilder
 from vencopy.core.gridModelers import GridModeler
 from vencopy.core.flexEstimators import FlexEstimator, WeekFlexEstimator
+
 # from vencopy.core.profileAggregators import ProfileAggregator
 # from vencopy.core.evaluators import Evaluator
-from vencopy.utils.globalFunctions import dumpReferenceData, loadConfigDict, createOutputFolders
+from vencopy.utils.globalFunctions import (
+    dumpReferenceData,
+    loadConfigDict,
+    createOutputFolders,
+)
 
 if __name__ == "__main__":
     # Set dataset and config to analyze, create output folders
     # datasetID options: 'MiD08' - 'MiD17' - 'KiD' - 'VF'
 
     basePath = Path(__file__).parent
-    configNames = ("globalConfig", "localPathConfig", "parseConfig", "diaryConfig",
-                   "gridConfig", "flexConfig", "aggregatorConfig", "evaluatorConfig")
+    configNames = (
+        "globalConfig",
+        "localPathConfig",
+        "parseConfig",
+        "diaryConfig",
+        "gridConfig",
+        "flexConfig",
+        "aggregatorConfig",
+        "evaluatorConfig",
+    )
     configDict = loadConfigDict(configNames, basePath=basePath)
     createOutputFolders(configDict=configDict)
-    
-    datasetID = configDict["globalConfig"]["dataset"]
-    if datasetID == "MiD17":
-        vpData = ParseMiD(configDict=configDict, datasetID=datasetID, debug=True)
-    elif datasetID == "KiD":
-        vpData = ParseKiD(configDict=configDict, datasetID=datasetID, debug=False)
-    elif datasetID == "VF":
-        vpData = ParseVF(configDict=configDict, datasetID=datasetID, debug=False)
+
+    vpData = parseData(configDict)
     vpData.process()
 
-    vpGrid = GridModeler(configDict=configDict, datasetID=datasetID, activities=vpData.activities, gridModel='simple')
+    vpGrid = GridModeler(
+        configDict=configDict,
+        datasetID=datasetID,
+        activities=vpData.activities,
+        gridModel="simple",
+    )
     vpGrid.assignGrid()
 
-    vpFlex = FlexEstimator(configDict=configDict, datasetID=datasetID, activities=vpGrid.activities)
+    vpFlex = FlexEstimator(
+        configDict=configDict, datasetID=datasetID, activities=vpGrid.activities
+    )
     vpFlex.estimateTechnicalFlexibility()
 
     # if 'postFlex' in configDict['globalConfig']['validation']['tags']:
@@ -53,13 +67,20 @@ if __name__ == "__main__":
     #                       tag='postFlex',
     #                       path=Path(configDict['globalConfig']['validation']['path']))
 
-    vpDiary = DiaryBuilder(configDict=configDict, datasetID=datasetID, activities=vpFlex.activities)
+    vpDiary = DiaryBuilder(
+        configDict=configDict, datasetID=datasetID, activities=vpFlex.activities
+    )
     vpDiary.createDiaries()
 
-    vpWDB = WeekDiaryBuilder(activities=vpGrid.activities, catCols=['areaType'])
+    vpWDB = WeekDiaryBuilder(activities=vpGrid.activities, catCols=["areaType"])
     vpWDB.composeWeekActivities(nWeeks=500, seed=42, replace=True)
 
-    vpWeFlex = WeekFlexEstimator(configDict=configDict, datasetID=datasetID, activities=vpWDB.activities, threshold=0.8)
+    vpWeFlex = WeekFlexEstimator(
+        configDict=configDict,
+        datasetID=datasetID,
+        activities=vpWDB.activities,
+        threshold=0.8,
+    )
     vpWeFlex.estimateTechnicalFlexibility()
 
     # profiles = ("drain")
