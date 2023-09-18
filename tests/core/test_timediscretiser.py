74c592894282dee5afa0a949710d6724b97f5459
Miorelli <Fabia.Miorelli@dlr.de>
2023-09-18 21:55:22 +0200

    adds skeleton for time discretiser testing

---
+++ tests/core/test_timediscretiser.py
@@ -0,0 +1,50 @@
+__version__ = "1.0.0"
+__maintainer__ = "Fabia Miorelli"
+__birthdate__ = "04.09.2023"
+__status__ = "dev"  # options are: dev, test, prod
+__license__ = "BSD-3-Clause"
+
+import pytest
+
+import pandas as pd
+
+from ...vencopy.core.diarybuilders import TimeDiscretiser
+
+# NOT TESTED: 
+
+
+@pytest.fixture
+def sample_configs():
+    configs = {
+        'user_config': {
+            'global': {
+                'dataset': "dataset1",
+                'debug': False,
+                'absolute_path': {
+                    'dataset1': '/path/to/dataset1',
+                    'dataset2': '/path/to/dataset2'
+                    }
+                },
+            'diarybuilders': {
+                'time_resolution': 15
+            }
+            },
+        'dev_config': {
+            'dataparsers': {
+                'data_variables': False
+                }
+            }
+        }
+    return configs
+
+# TODO: fix test
+def test_timediscretiser_init(sample_configs):
+    sample_activities_data = pd.DataFrame({})
+    builder = TimeDiscretiser(configs=sample_configs, activities=sample_activities_data)
+
+    assert builder.dev_config == sample_configs["dev_config"]
+    assert builder.user_config == sample_configs["user_config"]
+    assert builder.dataset == "dataset1"
+    assert builder.activities.equals(sample_activities_data)
+    assert builder.time_resolution == 15
+    assert builder.is_week_diary == False
